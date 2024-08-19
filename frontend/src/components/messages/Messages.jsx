3cb7c9a3de7b8d6b29baf53628d2d7cce098715d
masallam <masallam@ejada.com>
2024-08-19 15:31:30 +0200

    updated message view bug

--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -3,15 +3,13 @@ import Message from './Message';
 import useGetMessages from '../../hooks/useGetMessages';
 import MessageSkeleton from '../skeletons/MessageSkeleton';
 import useListenMessages from '../../hooks/useListenMessages';
+import useConversation from '../../zustand/useConversation';
 
 const Messages = () => {
+  const { selectedConversation } = useConversation();
   const { loading, messages } = useGetMessages();
   const lastMessageRef = useRef();
   useListenMessages();
-
-  // for useEffect
-  // [] -> runs once after the component mounts
-  // [count] -> runs when count changes
 
   useEffect(() => {
     setTimeout(() => {
@@ -20,22 +18,28 @@ const Messages = () => {
         block: 'end',
       });
     }, 5);
-  }, [messages]); // only runs when the values in the array changes
+  }, [messages]);
+
+  const filteredMessages = messages.filter(
+    (message) => message.senderId === selectedConversation?._id
+  );
+
   return (
-    <div className='px-4 flex-1 overflow-auto '>
-      {!loading &&
-        messages.length > 0 &&
+    <div className='px-4 flex-1 overflow-auto'>
+      {!loading && filteredMessages.length > 0 ? (
         messages.map((message) => (
           <div key={message.id} ref={lastMessageRef}>
             <Message key={message._id} message={message} />
           </div>
-        ))}
-      {loading && [...Array(8)].map((_, idx) => <MessageSkeleton key={idx} />)}
-      {!loading && messages.length === 0 && (
-        <p className='text-center mt-4 text-sm'>
-          Send a message to start the conversation
-        </p>
+        ))
+      ) : (
+        !loading && (
+          <p className='text-center mt-4 text-sm'>
+            Send a message to start the conversation
+          </p>
+        )
       )}
+      {loading && [...Array(8)].map((_, idx) => <MessageSkeleton key={idx} />)}
     </div>
   );
 };
